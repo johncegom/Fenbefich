1c0aec4c5d8fa1b9ec621b5aa5101f8e036bf001
johncegom <johncegom203@gmail.com>
2020-05-21 14:36:29 +0700

    Initiate project

---
+++ app/src/main/java/com/example/firebasegogo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.firebasegogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.firebase.client.Firebase;
+
+public class MainActivity extends AppCompatActivity {
+    private Firebase mRef;
+    private Button btn;
+    private EditText et;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+        mRef = new Firebase("https://fir-testing-d686c.firebaseio.com/Users");
+        btn = (Button) findViewById(R.id.button);
+        et = (EditText) findViewById(R.id.editText);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String value = et.getText().toString();
+//                Firebase mRefchild = mRef.child("Name");
+//                mRefchild.setValue(value);
+                mRef.push().setValue(value);
+            }
+        });
+    }
+}
