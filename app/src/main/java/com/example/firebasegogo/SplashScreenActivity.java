620958bf1b3becd72efd85740252db66d515dc64
tsukihi1 <64545762+tsukihi1@users.noreply.github.com>
2020-06-13 16:51:09 +0700

    Login func in splash screen

--- app/src/main/java/com/example/firebasegogo/SplashScreenActivity.java
+++ app/src/main/java/com/example/firebasegogo/SplashScreenActivity.java
@@ -1,49 +1,42 @@
 package com.example.firebasegogo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class SplashScreenActivity extends AppCompatActivity {
-
-    private static int SPLASH_SCREEN = 5000;
-
-    Animation topAnimation, bottomAnimation;
-    ImageView logoSplashScreen;
-    TextView appName;
+    ConstraintLayout constraintLayout;
+    LinearLayout linearLayout;
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            linearLayout.setVisibility(View.VISIBLE);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_screen);
-
-        //Load the animation
-        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
-        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
-
-        //Map
-        logoSplashScreen = (ImageView) findViewById(R.id.imageViewSplashScreen);
-        appName = (TextView) findViewById(R.id.textViewSplashScreen);
-
-        //Set animation for logo and text
-        logoSplashScreen.setAnimation(topAnimation);
-        appName.setAnimation(bottomAnimation);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, SPLASH_SCREEN);
+        constraintLayout = (ConstraintLayout) findViewById(R.id.cons_layout);
+        linearLayout = (LinearLayout) findViewById(R.id.linear_layout);
+        Animation hyperspaceJumpAnimation = AnimationUtils.loadAnimation(this,
+                R.anim.hyperspace_jump);
+        constraintLayout.startAnimation(hyperspaceJumpAnimation);
+        handler.postDelayed(runnable, 2000);
     }
 }
